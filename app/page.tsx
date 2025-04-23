89f73124fe6c153ba8e3982258378a67f460dcd5
Meman123 <87839716+Meman123@users.noreply.github.com>
2025-04-23 11:20:16 -0500

    fix resp

--- app/page.tsx
+++ app/page.tsx
@@ -4,39 +4,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div
-      className="flex items-center justify-center min-h-screen"
-      style={{ backgroundColor: "#14344b" }}
-    >
+    <div className="flex items-center justify-center min-h-screen bg-[#14344b] px-6 py-12">
       <Image
         src="/TundataLogo.svg"
         alt="Tundata Logo"
-        className="logo"
-        width={400} // Default width for desktop
-        height={400} // Default height for desktop
-        style={{ objectFit: "contain" }}
+        width={200} // Más pequeño en desktop
+        height={200}
+        className="w-full max-w-[160px] sm:max-w-[180px] md:max-w-[220px] lg:max-w-[240px] h-auto"
       />
-      <style jsx>{`
-        .logo {
-          width: 30vw;
-          max-width: 400px;
-          height: auto;
-        }
-
-        @media (min-width: 1024px) {
-          .logo {
-            width: 20vw;
-            max-width: 400px;
-          }
-        }
-
-        @media (max-width: 768px) {
-          .logo {
-            width: 50vw;
-            max-width: 200px;
-          }
-        }
-      `}</style>
     </div>
   );
 }
