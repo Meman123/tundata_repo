f920a5366f81a3c6a1cc52d6f74d319a599b0d00
Meman123 <87839716+Meman123@users.noreply.github.com>
2025-04-28 12:14:41 -0500

    header responsivness

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,19 +4,21 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full bg-[#14344B] flex justify-center items-center px-6 sm:px-12 md:px-24 py-4 sm:py-6 shadow-lg overflow-hidden relative z-10">
-      <div className="w-full max-w-7xl flex justify-center items-center">
-      <div className="w-[180px] sm:w-[220px] md:w-[250px] lg:w-[280px] xl:w-[320px]">
-          <Image
-            src="/LogoTundata.png"
-            alt="Tundata Logo"
-            width={542} // El tamaño real del asset (mejor calidad)
-            height={142}
-            className="w-full h-auto object-contain"
-            priority
-          />
-        </div>
-      </div>
-    </header>
+<header className="w-full bg-[#14344B] flex justify-center items-center px-6 py-3 shadow-lg overflow-hidden relative z-10">
+  <div className="w-full max-w-7xl flex justify-center items-center">
+    <div className="w-[150px] sm:w-[170px] md:w-[170px] lg:w-[190px] xl:w-[200px] 2xl:w-[200px]">
+      <Image
+        src="/LogoTundata.png"
+        alt="Tundata Logo"
+        width={542}
+        height={142}
+        className="w-full h-auto object-contain"
+        priority
+      />
+    </div>
+  </div>
+</header>
+
+
   );
 }
