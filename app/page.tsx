a1d7ff479e95fa257ce769f3b1f363e1dd5e17a1
Meman123 <87839716+Meman123@users.noreply.github.com>
2025-04-30 18:30:31 -0500

    quien quiere gobernar a duitama? v1

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import CandidatesSection from "../components/CandidatesSection";
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen flex flex-col bg-background overflow-x-hidden">
       <Header />
       <Hero />
+      <CandidatesSection />
     </main>
   );
 }
