e8e9233bc81b96acee0e2470658911938a7cbcad
Meman123 <87839716+Meman123@users.noreply.github.com>
2025-05-04 23:29:43 -0500

    restructure

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useRef } from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import CandidatesSection from "../components/CandidatesSection";
 import { useAvailableViewportHeight } from "../hooks/useAvailableViewportHeight";
 
 export default function Home() {
